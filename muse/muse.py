985b40d0696d58c6dea3d3526d5dd1025ab6f3fe
Brocef <ftf@debian>
2017-03-03 16:40:46 -0500

    Thing now compiles

--- muse/muse.py
+++ muse/muse.py
@@ -1,4 +1,3 @@
-import argparse
 import os
 import threading
 from math import log
@@ -11,8 +10,8 @@ import datetime
 import pickle
 from StringIO import StringIO
 
-from config import Config, STAGES
-from worker import worker, WORKER_TYPES, WORKER_TYPE_MAP
+from config import Config, ParseArgs, STAGES
+from worker import Worker, WORKER_TYPES, WORKER_TYPE_MAP
 from query import QUERY_TYPES, query
 from scout import Scout
 from doctor import Doctor
@@ -33,80 +32,6 @@ def similar(a, b):
 W_TYPE_STG_MAP = dict(zip(WORKER_TYPES, STAGES))
 STAGE_CLASSES = (Scout, Caravan, Doctor, Agent, Friend)
 STAGE_INDEX_MAP = {stage:index for (index, stage) in list(enumerate(STAGES))}
-
-term_re = re.compile(r'([^:]+):(Artist|Track|Album)')
-def ParseQueryTerm(term):
-    m = term_re.match(term)
-    if m:
-        return m.groups()
-    else:
-        return False
-
-query_re = re.compile(r'^(.+:)(\d+)$')
-gen_term_re = re.compile(r'(\s?([^{}]+)[\s:])|([{]([^{}]+)[}][\s:])')
-def ParseQuery(query):
-    if len(query) == 0:
-        err = 'The query cannot be an empty string!'
-        raise argparse.ArgumentTypeError(err)
-
-    m = query_re.match(query)
-    if m:
-        (query_line, num) = m.groups()
-        terms = []
-        clean_query = ''
-        for match in gen_term_re.finditer(query_line):
-            (_, q_raw_term, _, q_term) = match.groups()
-            if q_term == None:
-                clean_query = '%s %s' % (clean_query, q_raw_term)
-            elif q_raw_term == None:
-                p_term = ParseQueryTerm(q_term)
-                if p_term:
-                    terms.append(p_term)
-                    clean_query = '%s %s' % (clean_query, p_term[0])
-                else:
-                    err = 'The query term must be of the correct form {Query:[Artist,Track,Album]}'
-                    raise argparse.ArgumentTypeError(err)
-        clean_query = clean_query[1:]
-        return query(clean_query, terms, num)
-    else:
-        err = 'The query must be similar to the following form: {Queen:Artist} Lyric Videos:5'
-        raise argparse.ArgumentTypeError(err)
-
-def SafeParseQuery(query):
-    try:
-        return ParseQuery(query) != None
-    except Exception:
-        return False
-
-def ParseArgs():
-    parser = argparse.ArgumentParser(prog='Muse')
-    parser.add_argument('-D --home-dir', dest='home_dir', metavar='HOME_DIR_PATH', default=DEFAULT_HOME_DIR, help='The directory in which Muse stage folders are created (default %(default)s)')
-    parser.add_argument('-F --final-stage', dest='terminal_stage_name', choices=STAGES, default=STAGES[len(STAGES)-1], help='The stage %(prog)s should finish with (default %(default)s)')
-
-    subparsers = parser.add_subparsers(dest='mode', help='Subparser help')
-    discover_parser = subparsers.add_parser('discover', help='discover help')
-    #discover_parser.add_argument('-S --start-stage', dest='initial_stage', choices=STAGES, default=STAGES[0], help='The stage %(prog)s should start with (default %(default)r)')
-    discover_parser.add_argument('queries', nargs='+', type=ParseQuery, metavar='Query:Count', help='Search queries to use when scraping YouTube. You can also improve accuracy of the scraper by tagging your query type in the following form: "Untagged Query Terms {Tagged Query Terms:[Artist,Track,Album]}":N. For example, "{RZA:Artist} Lyric Videos":10 would start a search for a maximum of 10 candidate tracks of the Artist, RZA, with Lyric Videos appended to the query to give better context. Big Boi:10 for any video from the query Big Boi, with no tag heuristics enabled. You may have multiple tagged terms, but they may not be nested.')
-
-    compile_parser = subparsers.add_parser('compile', help='compile help')
-
-    resume_parser = subparsers.add_parser('resume', help='resume help')
-    resume_parser.add_argument('session', metavar='SESSION_NAME', type=str, help='Session name help')
-    #parser.add_argument('queries', nargs='+', metavar='Q:Type:N|SESSION NAME',
-    #        help='Search queries to use when scraping YouTube or the name of a previous session. You can also improve accuracy of the scraper by tagging your query type in the following form: Q:{Artist,Track,Album,*}:N. For example, RZA:Artist:10 would start a search for a maximum of 10 candidate tracks of the Artist, RZA. Big Boi:*:10 for any video from the query Big Boi.')
-    #parser.add_argument('-R --resume', dest='resume', action='store_const', const=True, default=False,
-    #        help='Resume a past session instead of starting a new one with a new query.')
-
-    args = parser.parse_args()
-    args.terminal_stage = STAGE_INDEX_MAP[args.terminal_stage_name]
-    #if not args.resume:
-    #    if not reduce(lambda acc, x: acc and SafeParseQuery(x), args.queries, True):
-    #        parser.print_help()
-    #        raise argparse.ArgumentTypeError('A query was malformed. Were you intending to resume a session instead?')
-
-    #if STAGE_INDEX_MAP[args.initial_stage] > STAGE_INDEX_MAP[args.terminal_stage]:
-    #    raise argparse.ArgumentTypeError('The final stage cannot happen before the start stage!')
-    return vars(args)
 
 class Muse(object):
     def __init__(self, config):
